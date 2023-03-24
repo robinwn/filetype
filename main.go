70f9c56715f58a2b32336fcb846bee7b36bf3fff
Robin Windels <robin.windels@gov.bc.ca>
2023-03-23 22:50:41 -0700

    Split the filename into parts

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 	"unicode"
 )
 
@@ -13,16 +14,24 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		filename := scanner.Text()
-		file, err := os.Open(filename)
+		fileh, err := os.Open(filename)
 		if err != nil {
 			log.Fatalf("error opening file %s: %s", filename, err)
 		}
-		defer file.Close()
+		defer fileh.Close()
 		var buffer [4]byte
 		var sbuffer [4]byte
-		_, err = io.ReadFull(file, buffer[:])
+		n := 0
+		n, err = io.ReadFull(fileh, buffer[:])
 		if err != nil {
-			log.Fatalf("error reading file %s: %s", filename, err)
+			if err == io.EOF || err == io.ErrUnexpectedEOF {
+				// Partial read, pad the buffer with zeros
+				for i := n; i < len(buffer); i++ {
+					buffer[i] = 0
+				}
+			} else {
+				log.Fatalf("error reading file %s: %s", filename, err)
+			}
 		}
 		for i, b := range buffer {
 			if unicode.IsPrint(rune(b)) {
@@ -31,7 +40,14 @@ func main() {
 				sbuffer[i] = '.'
 			}
 		}
-		fmt.Printf("%s,%x,%s\n", filename, buffer, sbuffer)
+		fileinfo, err := fileh.Stat()
+		if err != nil {
+			log.Fatalf("error getting file info for %s: %s", filename, err)
+		}
+		dir, file := filepath.Split(filename)
+		dir = filepath.Clean(dir)
+		ext := filepath.Ext(file)
+		fmt.Printf("%s,%s,%s,%d,%x,%s\n", dir, file[:len(file)-len(ext)], ext, fileinfo.Size(), buffer, sbuffer)
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatalf("error scanning input: %s", err)
