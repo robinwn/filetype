006e2623eca4b44fdd70bd2bbf4f38caec32662a
Robin Windels <robin.windels@gov.bc.ca>
2023-03-23 18:01:34 -0700

    Initial code and build

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		filename := scanner.Text()
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatalf("error opening file %s: %s", filename, err)
+		}
+		defer file.Close()
+		var buffer [4]byte
+		_, err = io.ReadFull(file, buffer[:])
+		if err != nil {
+			log.Fatalf("error reading file %s: %s", filename, err)
+		}
+		fmt.Printf("%s % x\n", filename, buffer)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("error scanning input: %s", err)
+	}
+}
