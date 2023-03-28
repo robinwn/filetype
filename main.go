8044657ef68073eff7e91657ef6c61e67c9fee0d
Robin Windels <robin.windels@gov.bc.ca>
2023-03-27 22:56:24 -0700

    Write properly formatted csv

--- main.go
+++ main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"bufio"
-	"fmt"
+	"encoding/csv"
+	"encoding/hex"
+	// "fmt"
 	"io"
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"unicode"
 )
 
 func main() {
+
+	writer := csv.NewWriter(os.Stdout)
+
 	scanner := bufio.NewScanner(os.Stdin)
+
 	for scanner.Scan() {
 		filename := scanner.Text()
 		fileh, err := os.Open(filename)
@@ -49,8 +56,14 @@ func main() {
 		dir, file := filepath.Split(filename)
 		dir = filepath.Clean(dir)
 		ext := filepath.Ext(file)
-		fmt.Printf("%q,%q,%q,%d,%x,%q\n", dir, file[:len(file)-len(ext)], ext, fileinfo.Size(), buffer, sbuffer)
+
+		cerr := writer.Write([]string{dir, file[:len(file)-len(ext)], ext, strconv.FormatInt(fileinfo.Size(), 10), hex.EncodeToString(buffer[:]), string(sbuffer[:])})
+		if cerr != nil {
+			panic(cerr)
+		}
+
 	}
+	writer.Flush()
 	if err := scanner.Err(); err != nil {
 		log.Fatalf("error scanning input: %s", err)
 	}
